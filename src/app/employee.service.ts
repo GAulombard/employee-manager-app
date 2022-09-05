8a11c6a3ad28835fa78e3aba34d8b863d6315c36
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2022-09-05 20:32:48 +0200

    wip

---
+++ src/app/employee.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiServeurUrl = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServeurUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServeurUrl}/employee/add`,employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServeurUrl}/employee/update`,employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServeurUrl}/employee/delete/${employeeId}`);
+  }
+
+}
